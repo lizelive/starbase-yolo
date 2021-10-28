27209f33f2ac99fde1b03553128d25339800d637
LizeLive <git@lize.live>
2021-10-28 11:47:33 -0700

    nice shit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # starbase-yolo
 I like code.
+
+
+# parts required
+
+1. a number of sps
+2. a display
---
+++ scripts/gen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sed 's/SPS1/SPS2/g' sps_1_basic.nolol > sps_2_basic.nolol
+sed 's/SPS1/SPS3/g' sps_1_basic.nolol > sps_3_basic.nolol
